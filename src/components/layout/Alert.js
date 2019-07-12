6ab975e0b3c7319022dc15f4012185177ffbcb7a
jim9112 <jim9112@gmail.com>
2019-07-12 12:42:57 -0400

    Fixed classname error in alerts.js

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Alert = ({alert}) => {
     return (
         alert != null && (
-            <div classname={`alert-${alert.type}`}>
+            <div className={`alert-${alert.type}`}>
                 <i className="fas fa-info-circle"></i> {alert.msg}
             </div>
         )
