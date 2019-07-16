b779044101dfb5a83b2cd02388bc05cb8c2b220d
jim9112 <jim9112@gmail.com>
2019-07-16 10:26:01 -0400

    Convert search to functional

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   }
   
   // search github users
@@ -34,6 +35,16 @@ class App extends Component {
 
     this.setState({ user: res.data, loading: false, alert: null});
   }
+
+  // get user repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true})
+    
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ repos: res.data, loading: false, alert: null});
+  }
+
   // clear users from state
   clearUsers = () => this.setState({users: [], loading: false, alert: null});
 
@@ -43,7 +54,7 @@ class App extends Component {
 
 
   render() {
-  const { loading, users, user } = this.state; 
+  const { loading, users, user, repos } = this.state; 
     
     
   return (
@@ -66,7 +77,14 @@ class App extends Component {
             )} />
             <Route exact path='/about' component={About} />
             <Route exact path='/user/:login' render={props => (
-              <User {...props} getUser={this.getUser} user={user} loading={loading} />
+              <User 
+                {...props} 
+                getUser={this.getUser} 
+                getUserRepos={this.getUserRepos} 
+                user={user}
+                repos={repos} 
+                loading={loading} 
+              />
             )}/>
           </Switch> 
         </div>
